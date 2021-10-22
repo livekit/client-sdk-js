a5092341e12bda41498b206d90390cca5d9153cb
David Zhao <david@davidzhao.com>
2021-10-22 14:11:07 -0700

    typo fix during track creation

--- src/room/track/create.ts
+++ src/room/track/create.ts
@@ -26,7 +26,7 @@ export async function createLocalTracks(
   options?: CreateLocalTracksOptions,
 ): Promise<Array<LocalTrack>> {
   if (!options) options = {};
-  if (options.audio === false) options.audio = {};
+  if (options.audio !== false) options.audio = {};
   if (options.video !== false) options.video = {};
 
   const constraints = LocalTrack.constraintsForOptions(options);
