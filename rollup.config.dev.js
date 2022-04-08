b9382d5e7411b1ba940a230ace63ada5eebfd8a0
Lukas <mail@lukasseiler.de>
2022-04-08 14:53:25 +0200

    prettify

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,10 +1,10 @@
 // @ts-check
-import glob from "glob";
-import path from "path";
-import typescript from '@rollup/plugin-typescript'
-import commonjs from '@rollup/plugin-commonjs'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import serve from "rollup-plugin-serve";
+import glob from 'glob';
+import path from 'path';
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
 
 const watcher = (globs) => ({
   buildStart() {
@@ -17,23 +17,21 @@ const watcher = (globs) => ({
 });
 
 export default {
-    input: 'example/sample.ts',
-    output: [
-      {
-        file: `example/build/bundle.js`,
-        format: 'esm',
-        strict: true,
-        sourcemap: true,
-        inlineDynamicImports: true,
-      },
-      ],
-      plugins: [
-          nodeResolve({browser: true, preferBuiltins: false}),
-          typescript({tsconfig: './example/tsconfig.json'}),
-          commonjs(),
-          serve({contentBase: 'example', open: true, port: 8080}),
-          watcher(['example/index.html', 'example/styles.css']),
-        ],
-        
-  };
-  
+  input: 'example/sample.ts',
+  output: [
+    {
+      file: `example/build/bundle.js`,
+      format: 'esm',
+      strict: true,
+      sourcemap: true,
+      inlineDynamicImports: true,
+    },
+  ],
+  plugins: [
+    nodeResolve({ browser: true, preferBuiltins: false }),
+    typescript({ tsconfig: './example/tsconfig.json' }),
+    commonjs(),
+    serve({ contentBase: 'example', open: true, port: 8080 }),
+    watcher(['example/index.html', 'example/styles.css']),
+  ],
+};
