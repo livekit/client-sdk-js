36bc27e84c7ca69d1b0ac34578db32d04771ee71
Lukas <mail@lukasseiler.de>
2022-05-06 15:42:23 +0200

    undo util changes

--- src/room/track/utils.ts
+++ src/room/track/utils.ts
@@ -83,13 +83,13 @@ export function constraintsForOptions(options: CreateLocalTracksOptions): MediaS
  */
 export async function detectSilence(track: AudioTrack, timeOffset = 200): Promise<boolean> {
   const ctx = getNewAudioContext();
-  if (ctx && track.mediaStream) {
+  if (ctx) {
     const analyser = ctx.createAnalyser();
     analyser.fftSize = 2048;
 
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
-    const source = ctx.createMediaStreamSource(track.mediaStream);
+    const source = ctx.createMediaStreamSource(new MediaStream([track.mediaStreamTrack]));
 
     source.connect(analyser);
     await sleep(timeOffset);
