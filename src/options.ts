8bed8d435ddd41aa132d8571bf97a5a995d2105a
lukasIO <mail@lukasseiler.de>
2022-05-13 19:20:25 +0200

    Remove deprecated code (#230)

--- src/options.ts
+++ src/options.ts
@@ -1,7 +1,5 @@
-import { LogLevel } from './logger';
 import {
   AudioCaptureOptions,
-  CreateLocalTracksOptions,
   TrackPublishDefaults,
   VideoCaptureOptions,
 } from './room/track/options';
@@ -73,84 +71,4 @@ export interface RoomConnectOptions {
    * publish only mode
    */
   publishOnly?: string;
-}
-
-/**
- * @deprecated use new Room([[RoomOptions]]) and room.connect([[RoomConnectOptions]]) instead
- *
- * if video or audio tracks are created as part of [[connect]], it'll automatically
- * publish those tracks to the room.
- */
-export interface ConnectOptions extends CreateLocalTracksOptions {
-  /** autosubscribe to room tracks upon connect, defaults to true */
-  autoSubscribe?: boolean;
-
-  /**
-   * publish only mode
-   */
-  publishOnly?: string;
-
-  /**
-   * see [[RoomOptions.adaptiveStream]]
-   */
-  adaptiveStream?: AdaptiveStreamSettings | boolean;
-
-  /**
-   * alias for adaptiveStream
-   * @deprecated
-   */
-  autoManageVideo?: boolean;
-
-  /**
-   * see [[RoomOptions.dynacast]]
-   */
-  dynacast?: boolean;
-
-  /** configures LiveKit internal log level */
-  logLevel?: LogLevel;
-
-  /**
-   * set ICE servers. When deployed correctly, LiveKit automatically uses the built-in TURN servers
-   */
-  iceServers?: RTCIceServer[];
-
-  /**
-   * use to override any RTCConfiguration options.
-   */
-  rtcConfig?: RTCConfiguration;
-
-  /**
-   * capture and publish audio track on connect, defaults to false
-   *
-   * If this option is used, you will not be notified if user denies capture permission.
-   */
-  audio?: boolean;
-
-  /**
-   * capture and publish video track on connect, defaults to false
-   *
-   * If this option is used, you will not be notified if user denies capture permission.
-   */
-  video?: boolean;
-
-  /**
-   * default options to use when capturing user's audio
-   */
-  audioCaptureDefaults?: AudioCaptureOptions;
-
-  /**
-   * default options to use when capturing user's video
-   */
-  videoCaptureDefaults?: VideoCaptureOptions;
-
-  /**
-   * default options to use when publishing tracks
-   */
-  publishDefaults?: TrackPublishDefaults;
-
-  /**
-   * should local tracks be stopped when they are unpublished. defaults to true
-   * set this to false if you would prefer to clean up unpublished local tracks manually.
-   */
-  stopLocalTrackOnUnpublish?: boolean;
 }
