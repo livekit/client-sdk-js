bbc9239aa0f6031d04a21c7022621b1005b90aa4
Lukas <mail@lukasseiler.de>
2022-06-02 13:49:56 +0200

    import version number directly from package.json at build time

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -4,6 +4,7 @@ import path from 'path';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 
@@ -32,6 +33,7 @@ export default {
     nodeResolve({ browser: true, preferBuiltins: false }),
     typescript({ tsconfig: './example/tsconfig.json' }),
     commonjs(),
+    json(),
     serve({ contentBase: 'example', open: true, port: 8080 }),
     watcher(['example/index.html', 'example/styles.css']),
     livereload(),
