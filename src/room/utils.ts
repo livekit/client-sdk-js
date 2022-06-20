ed2d790d7fbf5a9ad36610166c5d2889a78a420f
lukasIO <mail@lukasseiler.de>
2022-06-20 11:39:32 +0200

    Increase emtpy video stream size (#273)

--- src/room/utils.ts
+++ src/room/utils.ts
@@ -81,8 +81,9 @@ let emptyVideoStreamTrack: MediaStreamTrack | undefined;
 export function getEmptyVideoStreamTrack() {
   if (!emptyVideoStreamTrack) {
     const canvas = document.createElement('canvas');
-    canvas.width = 2;
-    canvas.height = 2;
+    // the canvas size is set to 16, because electron apps seem to fail with smaller values
+    canvas.width = 16;
+    canvas.height = 16;
     canvas.getContext('2d')?.fillRect(0, 0, canvas.width, canvas.height);
     // @ts-ignore
     const emptyStream = canvas.captureStream();
