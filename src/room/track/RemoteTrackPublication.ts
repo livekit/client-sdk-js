2237419a486ab40bbab092246d3c498a7bb764d0
David Zhao <dz@livekit.io>
2022-06-27 22:25:38 -0700

    Fix handling of permissions after resubscribe
    When a user subscribes a track, but then have permission revoked. The
    revoked state may be cached even when permissions are granted afterwards.
    Since subscription is an indicator that a user is allowed to subscribe
    to a track. We'll flip _allowed to true on subscribe.

--- src/room/track/RemoteTrackPublication.ts
+++ src/room/track/RemoteTrackPublication.ts
@@ -1,11 +1,11 @@
-import log from '../../logger';
-import { TrackInfo, VideoQuality } from '../../proto/livekit_models';
-import { UpdateSubscription, UpdateTrackSettings } from '../../proto/livekit_rtc';
-import { TrackEvent } from '../events';
-import RemoteVideoTrack from './RemoteVideoTrack';
-import { Track } from './Track';
-import { TrackPublication } from './TrackPublication';
-import { RemoteTrack } from './types';
+import log from '../../logger'
+import { TrackInfo, VideoQuality } from '../../proto/livekit_models'
+import { UpdateSubscription, UpdateTrackSettings } from '../../proto/livekit_rtc'
+import { TrackEvent } from '../events'
+import RemoteVideoTrack from './RemoteVideoTrack'
+import { Track } from './Track'
+import { TrackPublication } from './TrackPublication'
+import { RemoteTrack } from './types'
 
 export default class RemoteTrackPublication extends TrackPublication {
   track?: RemoteTrack;
@@ -125,6 +125,7 @@ export default class RemoteTrackPublication extends TrackPublication {
     return this.currentVideoQuality;
   }
 
+  /** @internal */
   setTrack(track?: Track) {
     if (this.track) {
       // unregister listener
@@ -164,6 +165,7 @@ export default class RemoteTrackPublication extends TrackPublication {
 
   protected handleEnded = (track: RemoteTrack) => {
     this.emit(TrackEvent.Ended, track);
+    this.setTrack(undefined);
   };
 
   protected get isAdaptiveStream(): boolean {
