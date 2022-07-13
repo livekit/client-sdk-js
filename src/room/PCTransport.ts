ab3b196e21362e2833c961984a63f40a81f155b3
cnderrauber <zengjie9004@gmail.com>
2022-07-13 12:29:40 +0800

    add nack for audio

--- src/room/PCTransport.ts
+++ src/room/PCTransport.ts
@@ -1,4 +1,5 @@
 import { debounce } from 'ts-debounce';
+import { parse, write } from 'sdp-transform';
 import log from '../logger';
 
 /** @internal */
@@ -88,25 +89,93 @@ export default class PCTransport {
     log.debug('starting to negotiate');
     const offer = await this.pc.createOffer(options);
 
-    // mung sdp for codec bitrate setting that can't apply by sendEncoding
-    this.trackBitrates.forEach((trackbr) => {
-      let sdp = offer.sdp ?? '';
-      const sidIndex = sdp.search(new RegExp(`msid.* ${trackbr.sid}`));
-      if (sidIndex < 0) {
-        return;
+    const sdpParsed = parse(offer.sdp ?? '');
+    sdpParsed.media.forEach((media) => {
+      if (media.type === 'audio') {
+        // found opus codec to add nack fb
+        let opusPayload = 0
+        media.rtp.some((rtp): boolean => {
+          if (rtp.codec === 'opus') {
+            opusPayload = rtp.payload;
+            return true;
+          }
+          return false;
+        })
+
+        // add nack rtcpfb if not exist
+        if (opusPayload > 0) {
+          if (!media.rtcpFb) {
+            media.rtcpFb = [];
+          }
+
+          if (!media.rtcpFb.some((fb) => fb.payload === opusPayload && fb.type === 'nack')) {
+            media.rtcpFb.push({
+              payload: opusPayload,
+              type: 'nack',
+            });
+          }
+        }
+      } else if (media.type === 'video') {
+        // mung sdp for codec bitrate setting that can't apply by sendEncoding
+        this.trackBitrates.some((trackbr): boolean => {
+          if (!media.msid || !media.msid.includes(trackbr.sid)) {
+            return false;
+          }
+
+          let codecPayload = 0;
+          media.rtp.some((rtp): boolean => {
+            if (rtp.codec.toUpperCase() === trackbr.codec.toUpperCase()) {
+              codecPayload = rtp.payload;
+              return true;
+            }
+            return false;
+          })
+
+          log.debug(`found ${media.msid} for ${trackbr.codec}`);
+
+          // add x-google-max-bitrate to fmtp line if not exist
+          if (codecPayload > 0) {
+            if (!media.fmtp.some((fmtp): boolean => {
+              if (fmtp.payload === codecPayload) {
+                if (!fmtp.config.includes('x-google-max-bitrate')) {
+                  fmtp.config += `;x-google-max-bitrate=${trackbr.maxbr}`
+                }
+                return true;
+              }
+              return false;
+            })) {
+              media.fmtp.push({
+                payload: codecPayload,
+                config: `x-google-max-bitrate=${trackbr.maxbr}`,
+              })
+            }
+          }
+
+          return true;
+        })
       }
-
-      const mlineStart = sdp.substring(0, sidIndex).lastIndexOf('m=');
-      const mlineEnd = sdp.indexOf('m=', sidIndex);
-      const mediaSection = sdp.substring(mlineStart, mlineEnd);
-
-      const mungedMediaSection = mediaSection.replace(
-        new RegExp(`a=rtpmap:(\\d+) ${trackbr.codec}/\\d+`, 'i'),
-        '$'.concat(`&\r\na=fmtp:$1 x-google-max-bitrate=${trackbr.maxbr}`), // Unity replaces '$&' by some random values when building ( Using concat as a workaround )
-      );
-      sdp = sdp.substring(0, mlineStart) + mungedMediaSection + sdp.substring(mlineEnd);
-      offer.sdp = sdp;
     });
+
+    offer.sdp = write(sdpParsed);
+
+    // this.trackBitrates.forEach((trackbr) => {
+    //   let sdp = offer.sdp ?? '';
+    //   const sidIndex = sdp.search(new RegExp(`msid.* ${trackbr.sid}`));
+    //   if (sidIndex < 0) {
+    //     return;
+    //   }
+
+    //   const mlineStart = sdp.substring(0, sidIndex).lastIndexOf('m=');
+    //   const mlineEnd = sdp.indexOf('m=', sidIndex);
+    //   const mediaSection = sdp.substring(mlineStart, mlineEnd);
+
+    //   const mungedMediaSection = mediaSection.replace(
+    //     new RegExp(`a=rtpmap:(\\d+) ${trackbr.codec}/\\d+`, 'i'),
+    //     '$'.concat(`&\r\na=fmtp:$1 x-google-max-bitrate=${trackbr.maxbr}`), // Unity replaces '$&' by some random values when building ( Using concat as a workaround )
+    //   );
+    //   sdp = sdp.substring(0, mlineStart) + mungedMediaSection + sdp.substring(mlineEnd);
+    //   offer.sdp = sdp;
+    // });
     this.trackBitrates = [];
 
     await this.pc.setLocalDescription(offer);
