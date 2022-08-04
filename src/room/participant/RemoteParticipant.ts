e927708af8b6b7451f000ef9e2b2324cfdda42d5
Lukas <mail@lukasseiler.de>
2022-08-04 17:17:26 +0200

    set name in participant constructors

--- src/room/participant/RemoteParticipant.ts
+++ src/room/participant/RemoteParticipant.ts
@@ -28,8 +28,8 @@ export default class RemoteParticipant extends Participant {
   }
 
   /** @internal */
-  constructor(signalClient: SignalClient, id: string, name?: string) {
-    super(id, name || '');
+  constructor(signalClient: SignalClient, sid: string, identity?: string, name?: string) {
+    super(sid, identity || '', name);
     this.signalClient = signalClient;
     this.tracks = new Map();
     this.audioTracks = new Map();
