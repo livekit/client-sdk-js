fe7467d60e5828f4c45f1f91586cef5ed4f06d97
Lukas <mail@lukasseiler.de>
2022-10-28 18:47:10 +0200

    remove pauseUpstream on ended

--- src/room/participant/LocalParticipant.ts
+++ src/room/participant/LocalParticipant.ts
@@ -964,7 +964,6 @@ export default class LocalParticipant extends Participant {
       });
       this.unpublishTrack(track);
     } else if (track.isUserProvided) {
-      await track.pauseUpstream();
       await track.mute();
     } else if (track instanceof LocalAudioTrack || track instanceof LocalVideoTrack) {
       try {
@@ -995,7 +994,6 @@ export default class LocalParticipant extends Participant {
         await track.restartTrack();
       } catch (e) {
         log.warn(`could not restart track, muting instead`);
-        await track.pauseUpstream();
         await track.mute();
       }
     }
