5e824b7edb1c39f4013ec48cef9226a7d6958555
lukasIO <mail@lukasseiler.de>
2022-12-06 08:30:30 +0100

    Use mutex for disconnect calls (#525)
    * use mutex for disconnection
    
    * changeset
    
    * remove buffer drain, as close call will do that already
    
    * simplify closePromise

---
+++ .changeset/gentle-lizards-smile.md
@@ -0,0 +1,5 @@
+---
+'livekit-client': patch
+---
+
+use mutex to prevent simultaneous calls to disconnect
--- src/room/utils.ts
+++ src/room/utils.ts
@@ -320,3 +320,38 @@ export function createAudioAnalyser(
 
   return { calculateVolume, analyser, cleanup };
 }
+
+export class Mutex {
+  private _locking: Promise<void>;
+
+  private _locks: number;
+
+  constructor() {
+    this._locking = Promise.resolve();
+    this._locks = 0;
+  }
+
+  isLocked() {
+    return this._locks > 0;
+  }
+
+  lock() {
+    this._locks += 1;
+
+    let unlockNext: () => void;
+
+    const willLock = new Promise<void>(
+      (resolve) =>
+        (unlockNext = () => {
+          this._locks -= 1;
+          resolve();
+        }),
+    );
+
+    const willUnlock = this._locking.then(() => unlockNext);
+
+    this._locking = this._locking.then(() => willLock);
+
+    return willUnlock;
+  }
+}
