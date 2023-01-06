0214a7c7af49d53730582738d2945a4f62231974
Lukas <mail@lukasseiler.de>
2023-01-06 14:42:46 +0100

    fix get user video

--- src/room/Room.ts
+++ src/room/Room.ts
@@ -1319,7 +1319,7 @@ class Room extends (EventEmitter as new () => TypedEmitter<RoomEventCallbacks>) 
         }),
         new LocalVideoTrack(
           publishOptions.useRealTracks
-            ? (await navigator.mediaDevices.getUserMedia({ audio: true })).getAudioTracks()[0]
+            ? (await navigator.mediaDevices.getUserMedia({ video: true })).getAudioTracks()[0]
             : createDummyVideoStreamTrack(
                 160 * participantOptions.aspectRatios[0] ?? 1,
                 160,
