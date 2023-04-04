3cf42c06c0b2e3541700e0174bc74396b389cf9c
lukasIO <mail@lukasseiler.de>
2023-04-04 16:31:45 +0200

    add web audio methods to local audio track

--- src/room/track/LocalAudioTrack.ts
+++ src/room/track/LocalAudioTrack.ts
@@ -1,7 +1,7 @@
 import log from '../../logger';
 import { TrackEvent } from '../events';
 import { AudioSenderStats, computeBitrate, monitorFrequency } from '../stats';
-import { isWeb } from '../utils';
+import { isWeb, supportsSetSinkId } from '../utils';
 import LocalTrack from './LocalTrack';
 import type { AudioCaptureOptions } from './options';
 import { Track } from './Track';
@@ -12,6 +12,18 @@ export default class LocalAudioTrack extends LocalTrack {
   stopOnMute: boolean = false;
 
   private prevStats?: AudioSenderStats;
+
+  private elementVolume: number | undefined;
+
+  private audioContext?: AudioContext;
+
+  private gainNode?: GainNode;
+
+  private sourceNode?: MediaStreamAudioSourceNode;
+
+  private webAudioPluginNodes: AudioNode[];
+
+  private sinkId?: string;
 
   /**
    *
@@ -26,6 +38,161 @@ export default class LocalAudioTrack extends LocalTrack {
   ) {
     super(mediaTrack, Track.Kind.Audio, constraints, userProvidedTrack);
     this.checkForSilence();
+    this.webAudioPluginNodes = [];
+  }
+
+  /**
+   * sets the volume for all attached audio elements
+   * note: only sets the volume locally
+   */
+  setVolume(volume: number) {
+    for (const el of this.attachedElements) {
+      if (this.audioContext) {
+        this.gainNode?.gain.setTargetAtTime(volume, 0, 0.1);
+      } else {
+        el.volume = volume;
+      }
+    }
+    this.elementVolume = volume;
+  }
+
+  /**
+   * calls setSinkId on all attached elements, if supported
+   * @param deviceId audio output device
+   */
+  async setSinkId(deviceId: string) {
+    this.sinkId = deviceId;
+    await Promise.all(
+      this.attachedElements.map((elm) => {
+        if (!supportsSetSinkId(elm)) {
+          return;
+        }
+        /* @ts-ignore */
+        return elm.setSinkId(deviceId) as Promise<void>;
+      }),
+    );
+  }
+
+  attach(): HTMLMediaElement;
+  attach(element: HTMLMediaElement): HTMLMediaElement;
+  attach(element?: HTMLMediaElement): HTMLMediaElement {
+    const needsNewWebAudioConnection = this.attachedElements.length === 0;
+    if (!element) {
+      element = super.attach();
+    } else {
+      super.attach(element);
+    }
+    if (this.elementVolume) {
+      element.volume = this.elementVolume;
+    }
+    if (this.sinkId && supportsSetSinkId(element)) {
+      /* @ts-ignore */
+      element.setSinkId(this.sinkId);
+    }
+    if (this.audioContext && needsNewWebAudioConnection) {
+      log.debug('using audio context mapping');
+      this.connectWebAudio(this.audioContext, element);
+      element.volume = 0;
+      element.muted = true;
+    }
+    return element;
+  }
+
+  /**
+   * Detaches from all attached elements
+   */
+  detach(): HTMLMediaElement[];
+
+  /**
+   * Detach from a single element
+   * @param element
+   */
+  detach(element: HTMLMediaElement): HTMLMediaElement;
+  detach(element?: HTMLMediaElement): HTMLMediaElement | HTMLMediaElement[] {
+    let detached: HTMLMediaElement | HTMLMediaElement[];
+    if (!element) {
+      detached = super.detach();
+      this.disconnectWebAudio();
+    } else {
+      detached = super.detach(element);
+      // if there are still any attached elements after detaching, connect webaudio to the first element that's left
+      // disconnect webaudio otherwise
+      if (this.audioContext) {
+        if (this.attachedElements.length > 0) {
+          this.connectWebAudio(this.audioContext, this.attachedElements[0]);
+        } else {
+          this.disconnectWebAudio();
+        }
+      }
+    }
+    return detached;
+  }
+
+  /**
+   * @internal
+   * @experimental
+   */
+  setAudioContext(audioContext: AudioContext | undefined) {
+    this.audioContext = audioContext;
+    if (audioContext && this.attachedElements.length > 0) {
+      this.connectWebAudio(audioContext, this.attachedElements[0]);
+    } else if (!audioContext) {
+      this.disconnectWebAudio();
+    }
+  }
+
+  /**
+   * @internal
+   * @experimental
+   * @param {AudioNode[]} nodes - An array of WebAudio nodes. These nodes should not be connected to each other when passed, as the sdk will take care of connecting them in the order of the array.
+   */
+  setWebAudioPlugins(nodes: AudioNode[]) {
+    this.webAudioPluginNodes = nodes;
+    if (this.attachedElements.length > 0 && this.audioContext) {
+      this.connectWebAudio(this.audioContext, this.attachedElements[0]);
+    }
+  }
+
+  private connectWebAudio(context: AudioContext, element: HTMLMediaElement) {
+    this.disconnectWebAudio();
+    // @ts-ignore attached elements always have a srcObject set
+    this.sourceNode = context.createMediaStreamSource(element.srcObject);
+    let lastNode: AudioNode = this.sourceNode;
+    this.webAudioPluginNodes.forEach((node) => {
+      lastNode.connect(node);
+      lastNode = node;
+    });
+    this.gainNode = context.createGain();
+    lastNode.connect(this.gainNode);
+    this.gainNode.connect(context.destination);
+
+    if (this.elementVolume) {
+      this.gainNode.gain.setTargetAtTime(this.elementVolume, 0, 0.1);
+    }
+
+    // try to resume the context if it isn't running already
+    if (context.state !== 'running') {
+      context
+        .resume()
+        .then(() => {
+          if (context.state !== 'running') {
+            this.emit(
+              TrackEvent.AudioPlaybackFailed,
+              new Error("Audio Context couldn't be started automatically"),
+            );
+          }
+        })
+        .catch((e) => {
+          this.emit(TrackEvent.AudioPlaybackFailed, e);
+        });
+    }
+  }
+
+  private disconnectWebAudio() {
+    this.gainNode?.disconnect();
+    this.sourceNode?.disconnect();
+    this.gainNode = undefined;
+    this.sourceNode = undefined;
   }
 
   async setDeviceId(deviceId: ConstrainDOMString) {
