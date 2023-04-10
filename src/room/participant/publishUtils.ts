29e1cb4fc5a52d9c81fbeb39b5ab970a9a9a8f88
cnderrauber <zengjie9004@gmail.com>
2023-04-10 15:39:20 +0800

    update vp9 bitrates

--- src/room/participant/publishUtils.ts
+++ src/room/participant/publishUtils.ts
@@ -249,6 +249,8 @@ export function determineAppropriateEncoding(
   if (codec) {
     switch (codec) {
       case 'av1':
+      case 'vp9':
+        encoding = {...encoding};
         encoding.maxBitrate = encoding.maxBitrate * 0.7;
         break;
       default:
