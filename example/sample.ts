84e33bd7186d9d7c43633c5af067908700d6c969
David Zhao <dz@livekit.io>
2023-07-05 22:31:09 -0700

    Fixed missed event listeners on MediaStreamTrack

--- example/sample.ts
+++ example/sample.ts
@@ -90,6 +90,7 @@ const appActions = {
         videoCodec: preferredCodec || 'vp8',
         dtx: true,
         red: true,
+        forceStereo: false,
       },
       videoCaptureDefaults: {
         resolution: VideoPresets.h720.resolution,
@@ -201,6 +202,13 @@ const appActions = {
       })
       .on(RoomEvent.ParticipantEncryptionStatusChanged, () => {
         updateButtonsForPublishState();
+      })
+      .on(RoomEvent.TrackStreamStateChanged, (pub, streamState, participant) => {
+        appendLog(
+          `stream state changed for ${pub.trackSid} (${
+            participant.identity
+          }) to ${streamState.toString()}`,
+        );
       });
 
     try {
