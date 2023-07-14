2a1e101351c2ae1883b432aceb5989bf4c545a75
lukasIO <mail@lukasseiler.de>
2023-07-14 13:56:17 +0200

    clarify vite usage

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { babel } from '@rollup/plugin-babel';
+/**
+ * Note: Vite is only used as a (fast) dev server.
+ * For building the library we invoke rollup directly.
+ **/
 import dns from 'dns';
-import { resolve } from 'path';
-import replace from 'rollup-plugin-re';
 import { defineConfig } from 'vite';
 import viteBabel from 'vite-plugin-babel';
 
@@ -29,42 +30,4 @@ export default defineConfig({
       },
     }),
   ],
-
-  build: {
-    minify: 'esbuild',
-    target: 'es2019',
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'Livekit Client SDK JS',
-      // the proper extensions will be added
-      fileName: 'livekit-client',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [],
-      output: {},
-      plugins: [
-        babel({
-          babelHelpers: 'bundled',
-          plugins: ['@babel/plugin-proposal-object-rest-spread'],
-          presets: ['@babel/preset-env'],
-          extensions: ['.js', '.ts', '.mjs'],
-        }),
-        replace({
-          patterns: [
-            {
-              // protobuf.js uses `eval` to determine whether a module is present or not
-              // in most modern browsers this will fail anyways due to CSP, and it's safer to just replace it with `undefined`
-              // until this PR is merged: https://github.com/protobufjs/protobuf.js/pull/1548
-              // related discussion: https://github.com/protobufjs/protobuf.js/issues/593
-              test: /eval.*\(moduleName\);/g,
-              replace: 'undefined;',
-            },
-          ],
-        }),
-      ],
-    },
-  },
 });
