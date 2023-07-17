655075584bda17e024ac62980f04dabba1ae658f
lukasIO <mail@lukasseiler.de>
2023-07-17 13:37:49 +0200

    add comment

--- .eslintrc.dist.cjs
+++ .eslintrc.dist.cjs
@@ -9,7 +9,7 @@ module.exports = {
   plugins: ['ecmascript-compat'],
   rules: {
     'ecmascript-compat/compat': [
-      'warn',
+      'warn', // FIXME once ecmascript-compat/compat supports feature checks or allows rule customization (https://github.com/robatwilliams/es-compat/issues/80) set this back to `error`
       {
         polyfills: [
           // rollup-common-js and tsproto have environment checks using `globalThis` which causes the compat check to fail on the output
