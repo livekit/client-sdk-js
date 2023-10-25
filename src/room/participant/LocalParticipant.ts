ce056d5f3f84985bcdcef13cf564d1f61df6f878
lukasIO <mail@lukasseiler.de>
2023-10-25 15:08:48 +0200

    revert enablesimulcast for this branch

--- src/room/participant/LocalParticipant.ts
+++ src/room/participant/LocalParticipant.ts
@@ -889,6 +889,7 @@ export default class LocalParticipant extends Participant {
         {
           codec: opts.videoCodec,
           cid: simulcastTrack.mediaStreamTrack.id,
+          enableSimulcastLayers: opts.simulcast,
         },
       ],
     });
