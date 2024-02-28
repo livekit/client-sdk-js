6e2711eadd0705aecdff6bf5bba73d65595b554d
lukasIO <mail@lukasseiler.de>
2024-02-28 16:20:18 +0100

    fix deviceId mixup

--- src/room/DeviceManager.ts
+++ src/room/DeviceManager.ts
@@ -83,13 +83,17 @@ export default class DeviceManager {
     // `default` devices will have the same groupId as the entry with the actual device id so we store the counts for each group id
     const groupIdCounts = new Map(devices.map((d) => [d.groupId, 0]));
 
-    devices.forEach((d) => groupIdCounts.set(d.deviceId, (groupIdCounts.get(d.deviceId) ?? 0) + 1));
+    devices.forEach((d) => groupIdCounts.set(d.groupId, (groupIdCounts.get(d.groupId) ?? 0) + 1));
+
+    console.log(groupIdCounts, devices);
 
     const device = devices.find(
       (d) =>
         (groupId === d.groupId || (groupIdCounts.get(d.groupId) ?? 0) > 1) &&
         d.deviceId !== defaultId,
     );
+
+    console.log(device);
 
     return device?.deviceId;
   }
