0c5259bceb564b0b9ab97e1569a1eabbe01c87c5
Hugh Nimmo-Smith <hughns@element.io>
2024-10-02 15:02:26 +0100

    Add unit test

---
+++ src/e2ee/worker/ParticipantKeyHandler.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from 'vitest';
+import { ParticipantKeyHandler } from './ParticipantKeyHandler';
+import { KEY_PROVIDER_DEFAULTS } from '../constants';
+
+describe('ParticipantKeyHandler', () => {
+  const participantIdentity = 'testParticipant';
+
+  it('keyringSize must be greater than 0', () => {
+    expect(() => {
+      new ParticipantKeyHandler(participantIdentity, { ...KEY_PROVIDER_DEFAULTS, keyringSize: 0 });
+    }).toThrowError(TypeError);
+  });
+
+  it('keyringSize must be less than 256', () => {
+    expect(() => {
+      new ParticipantKeyHandler(participantIdentity, { ...KEY_PROVIDER_DEFAULTS, keyringSize: 256 });
+    }).toThrowError(TypeError);
+  });
+
+  it('get and sets keys at an index', () => {
+    const keyHandler = new ParticipantKeyHandler(participantIdentity, { ...KEY_PROVIDER_DEFAULTS, keyringSize: 128 });
+    const keyA = { key: 'a' } as unknown as CryptoKey;
+    const keyB = { key: 'b' } as unknown as CryptoKey;
+    keyHandler.setKey(keyA, 0);
+    expect(keyHandler.getKeySet(0)).toEqual(keyA);
+    keyHandler.setKey(keyB, 0);
+    expect(keyHandler.getKeySet(0)).toEqual(keyB);
+  });
+});
