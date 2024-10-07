eb9043be711ceb3e188a878942f35fdec155e999
Ryan Lee <drdgvhbh@gmail.com>
2024-10-07 04:42:26 -0400

    chore: remove replace rollup plugin (#1274)
    The repo doesn't even use protobuf.js anymore and instead relies on @bufbuild/protobuf-es, so this can be safely removed. Before and after the esm bundle size was 788.4KB, which means the plugin was doing nothing.

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,6 @@ import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import del from 'rollup-plugin-delete';
-import replace from 'rollup-plugin-re';
 import typescript from 'rollup-plugin-typescript2';
 import packageJson from './package.json';
 
@@ -55,18 +54,6 @@ export default {
   plugins: [
     del({ targets: 'dist/*' }),
     typescript({ tsconfig: './tsconfig.json' }),
-    ...commonPlugins,
-    replace({
-      patterns: [
-        {
-          // protobuf.js uses `eval` to determine whether a module is present or not
-          // in most modern browsers this will fail anyways due to CSP, and it's safer to just replace it with `undefined`
-          // until this PR is merged: https://github.com/protobufjs/protobuf.js/pull/1548
-          // related discussion: https://github.com/protobufjs/protobuf.js/issues/593
-          test: /eval.*\(moduleName\);/g,
-          replace: 'undefined;',
-        },
-      ],
-    }),
+    ...commonPlugins
   ],
 };
