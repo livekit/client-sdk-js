cf1a07d91be26089f45b48caa496456566fb4ba5
lukasIO <mail@lukasseiler.de>
2025-02-12 17:48:28 +0100

    Only override deviceId constraint if it diverges (#1388)

--- src/room/track/LocalTrack.ts
+++ src/room/track/LocalTrack.ts
@@ -232,6 +232,7 @@ export default abstract class LocalTrack<
     ) {
       return true;
     }
+
     this._constraints.deviceId = deviceId;
 
     // when track is muted, underlying media stream track is stopped and
@@ -313,6 +314,7 @@ export default abstract class LocalTrack<
       if (!constraints) {
         constraints = this._constraints;
       }
+      const { deviceId, ...otherConstraints } = this._constraints;
       this.log.debug('restarting track with constraints', { ...this.logContext, constraints });
 
       const streamConstraints: MediaStreamConstraints = {
@@ -321,9 +323,9 @@ export default abstract class LocalTrack<
       };
 
       if (this.kind === Track.Kind.Video) {
-        streamConstraints.video = constraints;
+        streamConstraints.video = deviceId ? { deviceId } : true;
       } else {
-        streamConstraints.audio = constraints;
+        streamConstraints.audio = deviceId ? { deviceId } : true;
       }
 
       // these steps are duplicated from setMediaStreamTrack because we must stop
@@ -340,6 +342,7 @@ export default abstract class LocalTrack<
       // create new track and attach
       const mediaStream = await navigator.mediaDevices.getUserMedia(streamConstraints);
       const newTrack = mediaStream.getTracks()[0];
+      await newTrack.applyConstraints(otherConstraints);
       newTrack.addEventListener('ended', this.handleEnded);
       this.log.debug('re-acquired MediaStreamTrack', this.logContext);
 
