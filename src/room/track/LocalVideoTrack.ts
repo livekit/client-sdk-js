5aca1a98a06b84310701c489a9a68f6990c119d2
cnderrauber <zengjie9004@gmail.com>
2025-03-28 11:39:27 +0800

    Enable dynamic scalability modes for svc dynacast

--- src/room/track/LocalVideoTrack.ts
+++ src/room/track/LocalVideoTrack.ts
@@ -460,13 +460,12 @@ async function setPublishingLayersForSender(
     1. chrome 113: when switching to up layer with scalability Mode change, it will generate a
           low resolution frame and recover very quickly, but noticable
     2. livekit sfu: additional pli request cause video frozen for a few frames, also noticable */
-    const closableSpatial = false;
     /* @ts-ignore */
-    if (closableSpatial && encodings[0].scalabilityMode) {
+    if (encodings[0].scalabilityMode) {
       // svc dynacast encodings
       const encoding = encodings[0];
       /* @ts-ignore */
-      // const mode = new ScalabilityMode(encoding.scalabilityMode);
+      const mode = new ScalabilityMode(encoding.scalabilityMode);
       let maxQuality = ProtoVideoQuality.OFF;
       qualities.forEach((q) => {
         if (q.enabled && (maxQuality === ProtoVideoQuality.OFF || q.quality > maxQuality)) {
@@ -479,22 +478,25 @@ async function setPublishingLayersForSender(
           encoding.active = false;
           hasChanged = true;
         }
-      } else if (!encoding.active /* || mode.spatial !== maxQuality + 1*/) {
+      } else if (!encoding.active || mode.spatial !== maxQuality + 1) {
         hasChanged = true;
         encoding.active = true;
-        /*
-        @ts-ignore
-        const originalMode = new ScalabilityMode(senderEncodings[0].scalabilityMode)
+        /* @ts-ignore */
+        const originalMode = new ScalabilityMode(senderEncodings[0].scalabilityMode);
         mode.spatial = maxQuality + 1;
         mode.suffix = originalMode.suffix;
         if (mode.spatial === 1) {
           // no suffix for L1Tx
           mode.suffix = undefined;
         }
-        @ts-ignore
+        /* @ts-ignore */
         encoding.scalabilityMode = mode.toString();
         encoding.scaleResolutionDownBy = 2 ** (2 - maxQuality);
-      */
+        if (senderEncodings[0].maxBitrate) {
+          encoding.maxBitrate =
+            senderEncodings[0].maxBitrate /
+            (encoding.scaleResolutionDownBy * encoding.scaleResolutionDownBy);
+        }
       }
     } else {
       // simulcast dynacast encodings
