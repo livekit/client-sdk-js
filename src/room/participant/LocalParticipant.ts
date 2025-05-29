76df0f203b5b39aac4fccecee1cf66ac182b6c76
lukasIO <mail@lukasseiler.de>
2025-05-29 13:49:55 +0200

    fix: ensure signal connect future is reset after disconnecting from room (#1533)

--- src/room/participant/LocalParticipant.ts
+++ src/room/participant/LocalParticipant.ts
@@ -260,6 +260,8 @@ export default class LocalParticipant extends Participant {
       .on(EngineEvent.Disconnected, this.handleDisconnected)
       .on(EngineEvent.SignalRequestResponse, this.handleSignalRequestResponse)
       .on(EngineEvent.DataPacketReceived, this.handleDataPacket);
+
+    this.signalConnectedFuture = undefined;
   }
 
   private handleReconnecting = () => {
