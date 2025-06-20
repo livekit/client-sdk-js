d869f2188ddcf679f15d828bff1c611b52d8099f
lukasIO <mail@lukasseiler.de>
2025-06-20 10:31:39 +0200

    wait for participant in onTrack

--- src/room/Room.ts
+++ src/room/Room.ts
@@ -1412,7 +1412,7 @@ class Room extends (EventEmitter as new () => TypedEmitter<RoomEventCallbacks>) 
     this.maybeCreateEngine();
   }
 
-  private onTrackAdded(
+  private async onTrackAdded(
     mediaTrack: MediaStreamTrack,
     stream: MediaStream,
     receiver: RTCRtpReceiver,
@@ -1458,16 +1458,34 @@ class Room extends (EventEmitter as new () => TypedEmitter<RoomEventCallbacks>) 
       return;
     }
 
-    const participant = Array.from(this.remoteParticipants.values()).find(
-      (p) => p.sid === participantSid,
-    ) as RemoteParticipant | undefined;
+    const findParticipant = () =>
+      Array.from(this.remoteParticipants.values()).find((p) => p.sid === participantSid) as
+        | RemoteParticipant
+        | undefined;
+
+    let participant = findParticipant();
 
     if (!participant) {
-      this.log.error(
-        `Tried to add a track for a participant, that's not present. Sid: ${participantSid}`,
-        this.logContext,
-      );
-      return;
+      const waitForParticipant = async () => {
+        const start = performance.now();
+        while (performance.now() - start < 300) {
+          await sleep(20);
+          const p = findParticipant();
+          if (p) {
+            return p;
+          }
+        }
+      };
+      participant = await waitForParticipant();
+
+      if (!participant) {
+        // participant still not found after waiting for it, giving up
+        this.log.error(
+          `Tried to add a track for a participant, that's not present. Sid: ${participantSid}`,
+          this.logContext,
+        );
+        return;
+      }
     }
 
     let adaptiveStreamSettings: AdaptiveStreamSettings | undefined;
