595d4fbeb44b592d8b347dd141524f5f95e38fe6
cnderrauber <zengjie9004@gmail.com>
2025-07-02 15:46:51 +0800

    Support H265 (#1576)

--- src/room/track/options.ts
+++ src/room/track/options.ts
@@ -392,7 +392,7 @@ export interface AudioPreset {
 
 const backupCodecs = ['vp8', 'h264'] as const;
 
-export const videoCodecs = ['vp8', 'h264', 'vp9', 'av1'] as const;
+export const videoCodecs = ['vp8', 'h264', 'vp9', 'av1', 'h265'] as const;
 
 export type VideoCodec = (typeof videoCodecs)[number];
 
