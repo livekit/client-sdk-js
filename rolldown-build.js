435c7ee3a6be86fcbb992425e795e1d902b4e77c
lukasIO <mail@lukasseiler.de>
2025-07-02 11:01:17 +0200

    built in dts

--- rolldown-build.js
+++ rolldown-build.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { rolldown } from 'rolldown';
+import { dts } from 'rolldown-plugin-dts';
 import del from 'rollup-plugin-delete';
 import { minify } from 'rollup-plugin-esbuild';
 import packageJson from './package.json' with { type: 'json' };
@@ -10,39 +11,58 @@ export function kebabCaseToPascalCase(string = '') {
   );
 }
 
+const inputPlugins = [
+  del({ targets: 'dist/*' }),
+  dts({
+    tsconfig: 'tsconfig.json',
+  }),
+];
+
 const clientBundle = await rolldown({
   input: 'src/index.ts',
-  plugins: [del({ targets: 'dist/*' })],
+  plugins: inputPlugins,
 });
 
 const workerBundle = await rolldown({
   input: 'src/e2ee/worker/e2ee.worker.ts',
-  plugins: [del({ targets: 'dist/*' })],
+  plugins: inputPlugins,
 });
 
 await Promise.all([
   clientBundle.write({
-    file: `dist/${packageJson.name}.esm.mjs`,
+    dir: 'dist',
+    entryFileNames: (chunkInfo) => {
+      if (chunkInfo.name === 'index') {
+        return `${packageJson.name}.esm.mjs`;
+      }
+      return `${packageJson.name}.${chunkInfo.name}.mjs`;
+    },
     format: 'esm',
     sourcemap: true,
   }),
-  clientBundle.write({
-    file: `dist/${packageJson.name}.umd.js`,
-    format: 'umd',
-    sourcemap: true,
-    name: kebabCaseToPascalCase(packageJson.name),
-    plugins: [minify()],
-  }),
+  // clientBundle.write({
+  //   dir: 'dist',
+  //   format: 'umd',
+  //   sourcemap: true,
+  //   name: kebabCaseToPascalCase(packageJson.name),
+  //   plugins: [minify()],
+  // }),
   workerBundle.write({
-    file: `dist/${packageJson.name}.e2ee.worker.mjs`,
+    dir: 'dist',
+    entryFileNames: (chunkInfo) => {
+      if (chunkInfo.name === 'e2ee.worker') {
+        return `${packageJson.name}.e2ee.worker.mjs`;
+      }
+      return chunkInfo.name;
+    },
     format: 'esm',
     sourcemap: true,
   }),
-  workerBundle.write({
-    file: `dist/${packageJson.name}.e2ee.worker.js`,
-    format: 'umd',
-    sourcemap: true,
-    name: kebabCaseToPascalCase(packageJson.name) + '.e2ee.worker',
-    plugins: [minify()],
-  }),
+  // workerBundle.write({
+  //   dir: 'dist',
+  //   format: 'umd',
+  //   sourcemap: true,
+  //   name: kebabCaseToPascalCase(packageJson.name) + '.e2ee.worker',
+  //   plugins: [minify()],
+  // }),
 ]);
