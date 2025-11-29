b2569278d225343707b21430f7a2fdffaadd71f4
lukasIO <mail@lukasseiler.de>
2025-11-29 15:14:16 +0100

    more helpers

--- src/utils/TypedPromise.ts
+++ src/utils/TypedPromise.ts
@@ -1,3 +1,5 @@
+type InferErrors<T> = T extends TypedPromise<any, infer E> ? E : never;
+
 export default class TypedPromise<T, E extends Error> extends Promise<T> {
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(
@@ -12,7 +14,26 @@ export default class TypedPromise<T, E extends Error> extends Promise<T> {
     return super.catch(onrejected);
   }
 
-  static reject<E extends Error>(reason: E) {
-    return super.reject(reason) as TypedPromise<never, E>;
+  static reject<E extends Error>(reason: E): TypedPromise<never, E> {
+    return super.reject(reason);
+  }
+
+  static all<T extends readonly unknown[] | []>(
+    values: T,
+  ): TypedPromise<{ -readonly [P in keyof T]: Awaited<T[P]> }, InferErrors<T[number]>> {
+    return super.all(values) as any;
+  }
+
+  static race<T extends readonly (TypedPromise<any, any> | any)[]>(
+    values: T,
+  ): TypedPromise<
+    T[number] extends TypedPromise<infer U, any>
+      ? U
+      : T[number] extends PromiseLike<infer U>
+        ? U
+        : Awaited<T[number]>,
+    InferErrors<T[number]>
+  > {
+    return super.race(values);
   }
 }
