ac182a35e13b0d5d935ff598f82e099a6149c8e9
Ryan Gaus <rsg1egoman@gmail.com>
2026-01-27 15:40:25 +1300

    fix: update docs to clarify other client client HKDF support

--- src/e2ee/KeyProvider.ts
+++ src/e2ee/KeyProvider.ts
@@ -91,8 +91,10 @@ export class ExternalE2EEKeyProvider extends BaseKeyProvider {
 
   /**
    * Accepts a passphrase that's used to create the crypto keys.
-   * When passing in a string, PBKDF2 is used.
-   * When passing in an Array buffer of cryptographically random numbers, HKDF is being used. (recommended)
+   * When passing in a string, PBKDF2 is used. (recommended)
+   * When passing in an Array buffer of cryptographically random numbers, HKDF is being used.
+   *
+   * Note that not all client sdks support HKDF.
    * @param key
    */
   async setKey(key: string | ArrayBuffer) {
