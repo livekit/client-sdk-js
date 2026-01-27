d4d1093291a32a488c40f408dff93b2a69b26d0e
Ryan Gaus <rsg1egoman@gmail.com>
2026-01-28 12:55:48 +1300

    feat: add depacketizer tests

---
+++ src/room/data-track/depacketizer.test.ts
@@ -0,0 +1,191 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { DataTrackHandle } from './handle';
+import { DataTrackPacket, DataTrackPacketHeader, FrameMarker } from './packet';
+import { DataTrackTimestamp, WrapAroundUnsignedInt } from './utils';
+import { DataTrackDepacketizer } from './depacketizer';
+
+const EMPTY_EXTENSIONS_JSON = { userTimestamp: null, e2ee: null };
+
+describe('DataTrackDepacketizer', () => {
+  it('should depacketize a single packet', () => {
+    const depacketizer = new DataTrackDepacketizer();
+    const packet = new DataTrackPacket(
+      new DataTrackPacketHeader({
+        marker: FrameMarker.Single,
+        trackHandle: DataTrackHandle.fromNumber(101),
+        sequence: WrapAroundUnsignedInt.u16(0),
+        frameNumber: WrapAroundUnsignedInt.u16(103),
+        timestamp: DataTrackTimestamp.fromRtpTicks(104),
+      }),
+      new Uint8Array(0),
+    );
+
+    const result = depacketizer.push(packet);
+
+    expect(result.dropError).toBeNull();
+    expect(result.frame!.payload).toStrictEqual(new Uint8Array(0));
+    expect(result.frame!.extensions.toJSON()).toStrictEqual(EMPTY_EXTENSIONS_JSON);
+  });
+
+  it.each([0, 8, DataTrackDepacketizer.MAX_BUFFER_PACKETS - 2])('should depacketize a multi packet message', (interPacketCount) => {
+    const depacketizer = new DataTrackDepacketizer();
+
+    const packetPayload = new Uint8Array(8);
+    const packetHeaderParams = {
+      /* no marker */
+      trackHandle: DataTrackHandle.fromNumber(101),
+      sequence: WrapAroundUnsignedInt.u16(0),
+      frameNumber: WrapAroundUnsignedInt.u16(103),
+      timestamp: DataTrackTimestamp.fromRtpTicks(104),
+    };
+
+    const startPacket = new DataTrackPacket(
+      new DataTrackPacketHeader({ ...packetHeaderParams, marker: FrameMarker.Start }),
+      packetPayload,
+    );
+    const startPacketResult = depacketizer.push(startPacket);
+    expect(startPacketResult.frame).toBeNull();
+    expect(startPacketResult.dropError).toBeNull();
+
+    for (let i = 0; i < interPacketCount; i += 1) {
+      const interPacket = new DataTrackPacket(
+        new DataTrackPacketHeader({ ...packetHeaderParams, marker: FrameMarker.Inter, sequence: WrapAroundUnsignedInt.u16(i) }),
+        packetPayload,
+      );
+      const interPacketResult = depacketizer.push(interPacket);
+      expect(interPacketResult.frame).toBeNull();
+      expect(interPacketResult.dropError).toBeNull();
+    }
+
+    const finalPacket = new DataTrackPacket(
+      new DataTrackPacketHeader({ ...packetHeaderParams, marker: FrameMarker.Final, sequence: WrapAroundUnsignedInt.u16(interPacketCount) }),
+      packetPayload,
+    );
+    const finalPacketResult = depacketizer.push(finalPacket);
+    expect(finalPacketResult.dropError).toBeNull();
+
+    expect(finalPacketResult.frame!.extensions.toJSON()).toStrictEqual(EMPTY_EXTENSIONS_JSON);
+    expect(finalPacketResult.frame!.payload.byteLength).toStrictEqual(packetPayload.byteLength * (1 /* start */ + interPacketCount + 1 /* final */));
+  });
+
+  it('should throw "interrupted" when frame number changes midway through depacketizing', () => {
+    const depacketizer = new DataTrackDepacketizer();
+    const packetA = new DataTrackPacket(
+      new DataTrackPacketHeader({
+        marker: FrameMarker.Start,
+        trackHandle: DataTrackHandle.fromNumber(1),
+        sequence: WrapAroundUnsignedInt.u16(0),
+        frameNumber: WrapAroundUnsignedInt.u16(5 /* starts on frame 5 */),
+        timestamp: DataTrackTimestamp.fromRtpTicks(0),
+      }),
+      new Uint8Array(8),
+    );
+
+    const resultA = depacketizer.push(packetA);
+    expect(resultA.frame).toBeNull();
+    expect(resultA.dropError).toBeNull();
+
+    // Now feed in a packet with a different frame number:
+    const nextFrameNumber = packetA.header.frameNumber.value + 1;
+    const packetB = new DataTrackPacket(
+      new DataTrackPacketHeader({
+        marker: FrameMarker.Start,
+        trackHandle: DataTrackHandle.fromNumber(1),
+        sequence: WrapAroundUnsignedInt.u16(0),
+        frameNumber: WrapAroundUnsignedInt.u16(nextFrameNumber),
+        timestamp: DataTrackTimestamp.fromRtpTicks(0),
+      }),
+      new Uint8Array(8),
+    );
+
+    const resultB = depacketizer.push(packetB);
+    expect(resultB.frame).toBeNull();
+    expect(resultB.dropError!.message).toStrictEqual("Frame 5 dropped: Interrupted by the start of a new frame");
+    expect(resultB.dropError!.frameNumber).toStrictEqual(packetA.header.frameNumber.value);
+  });
+
+  it('should throw "incomplete" when final packet comes too early', () => {
+    const depacketizer = new DataTrackDepacketizer();
+
+    const packetPayload = new Uint8Array(8);
+    const packetHeaderParams = {
+      /* no marker */
+      trackHandle: DataTrackHandle.fromNumber(101),
+      sequence: WrapAroundUnsignedInt.u16(0),
+      frameNumber: WrapAroundUnsignedInt.u16(103),
+      timestamp: DataTrackTimestamp.fromRtpTicks(104),
+    };
+
+    const startPacket = new DataTrackPacket(
+      new DataTrackPacketHeader({ ...packetHeaderParams, marker: FrameMarker.Start }),
+      packetPayload,
+    );
+    const startPacketResult = depacketizer.push(startPacket);
+    expect(startPacketResult.frame).toBeNull();
+    expect(startPacketResult.dropError).toBeNull();
+
+    const finalPacket = new DataTrackPacket(
+      new DataTrackPacketHeader({ ...packetHeaderParams, marker: FrameMarker.Final, sequence: WrapAroundUnsignedInt.u16(3) }),
+      packetPayload,
+    );
+    const finalPacketResult = depacketizer.push(finalPacket);
+    expect(finalPacketResult.frame).toBeNull();
+    expect(finalPacketResult.dropError!.message).toStrictEqual("Frame 103 dropped: Not all packets received before final packet. Received 2 packets, expected 4 packets.");
+    expect(finalPacketResult.dropError!.frameNumber).toStrictEqual(startPacket.header.frameNumber.value);
+  });
+
+  it('should throw "unknownFrame" when a non single packet frame does not start with a "start" packet', () => {
+    const depacketizer = new DataTrackDepacketizer();
+
+    const packet = new DataTrackPacket(
+      new DataTrackPacketHeader({ 
+        marker: FrameMarker.Inter,
+        trackHandle: DataTrackHandle.fromNumber(101),
+        sequence: WrapAroundUnsignedInt.u16(0),
+        frameNumber: WrapAroundUnsignedInt.u16(103),
+        timestamp: DataTrackTimestamp.fromRtpTicks(104),
+      }),
+      new Uint8Array(8),
+    );
+    expect(() => depacketizer.push(packet)).toThrowError("Frame 103 dropped: Initial packet was never received.");
+  });
+
+  it('should throw "bufferFull" when too many packets have been sent', () => {
+    const depacketizer = new DataTrackDepacketizer();
+
+    const packetPayload = new Uint8Array(8);
+    const packetHeaderParams = {
+      /* no marker */
+      trackHandle: DataTrackHandle.fromNumber(101),
+      sequence: WrapAroundUnsignedInt.u16(0),
+      frameNumber: WrapAroundUnsignedInt.u16(103),
+      timestamp: DataTrackTimestamp.fromRtpTicks(104),
+    };
+
+    const startPacket = new DataTrackPacket(
+      new DataTrackPacketHeader({ ...packetHeaderParams, marker: FrameMarker.Start }),
+      packetPayload,
+    );
+    const startPacketResult = depacketizer.push(startPacket);
+    expect(startPacketResult.frame).toBeNull();
+    expect(startPacketResult.dropError).toBeNull();
+
+    for (let i = 0; i < DataTrackDepacketizer.MAX_BUFFER_PACKETS; i += 1) {
+      const interPacket = new DataTrackPacket(
+        new DataTrackPacketHeader({ ...packetHeaderParams, marker: FrameMarker.Inter, sequence: WrapAroundUnsignedInt.u16(i) }),
+        packetPayload,
+      );
+      const interPacketResult = depacketizer.push(interPacket);
+      expect(interPacketResult.frame).toBeNull();
+      expect(interPacketResult.dropError).toBeNull();
+    }
+
+    // Send one final inter packet (so one more than the max), and make sure the error case gets hit
+    const extraInterPacket = new DataTrackPacket(
+      new DataTrackPacketHeader({ ...packetHeaderParams, marker: FrameMarker.Final, sequence: WrapAroundUnsignedInt.u16(DataTrackDepacketizer.MAX_BUFFER_PACKETS) }),
+      packetPayload,
+    );
+    expect(() => depacketizer.push(extraInterPacket)).toThrowError("Frame 103 dropped: Reorder buffer is full.");
+  });
+});
