690e1b78d0716427633af3d3e124a4179815771e
Ryan Gaus <rsg1egoman@gmail.com>
2026-03-10 12:50:30 -0400

    fix: make functions into arrow fns to work around `this` being unset

--- src/room/Room.ts
+++ src/room/Room.ts
@@ -2396,7 +2396,7 @@ class Room extends (EventEmitter as new () => TypedEmitter<RoomEventCallbacks>) 
     }
   }
 
-  private getRemoteParticipantClientProtocol(identity: Participant["identity"]) {
+  private getRemoteParticipantClientProtocol = (identity: Participant["identity"]) => {
     return this.remoteParticipants.get(identity)?.clientProtocol ?? CLIENT_PROTOCOL_DEFAULT;
   }
 
