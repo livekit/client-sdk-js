350e51f231cdc4386ea104ac64e65b9a32393864
Ryan Gaus <ryan.gaus@livekit.io>
2026-07-15 13:16:31 -0400

    fix: remove this.deviceChangeCleanupController?.abort() from disconnect

--- src/room/Room.ts
+++ src/room/Room.ts
@@ -192,8 +192,6 @@ class Room extends (EventEmitter as new () => TypedEmitter<RoomEventCallbacks>) 
   /** used for aborting pending connections to a LiveKit server */
   private abortController?: AbortController;
 
-  /** used to remove the `devicechange` listener registered in the constructor */
-  private deviceChangeCleanupController?: AbortController;
 
   /** future holding client initiated connection attempt */
   private connectFuture?: Future<void, Error>;
@@ -378,7 +376,6 @@ class Room extends (EventEmitter as new () => TypedEmitter<RoomEventCallbacks>) 
 
     if (isWeb()) {
       const cleanupController = new AbortController();
-      this.deviceChangeCleanupController = cleanupController;
       let onDeviceChange: () => void;
 
       if (Room.cleanupRegistry) {
@@ -1870,7 +1867,6 @@ class Room extends (EventEmitter as new () => TypedEmitter<RoomEventCallbacks>) 
         window.removeEventListener('beforeunload', this.onPageLeave);
         window.removeEventListener('pagehide', this.onPageLeave);
         window.removeEventListener('freeze', this.onPageLeave);
-        this.deviceChangeCleanupController?.abort();
       }
     } finally {
       this.setAndEmitConnectionState(ConnectionState.Disconnected);
