47e090b221aa7e822652a405dc64dc8f15fd6aad
lukasIO <mail@lukasseiler.de>
2026-09-07 12:25:20 +0200

    use pc manager mode

--- src/room/RTCEngine.ts
+++ src/room/RTCEngine.ts
@@ -665,7 +665,7 @@ export default class RTCEngine extends (EventEmitter as new () => TypedEventEmit
       });
       // in dual PC mode the publisher answer carries no mapping (the server's publisher
       // transport has no sending tracks) and must not clobber the subscriber offer mapping
-      if (this.options.singlePeerConnection) {
+      if (this.pcManager.mode === 'publisher-only') {
         this.midToTrackId = midToTrackId;
       }
       await this.pcManager.setPublisherAnswer(sd, offerId);
